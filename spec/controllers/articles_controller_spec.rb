fdafcc732db236999c90965bf2fcd3d3d2b1b874
Josh Susser <josh@hasmanythrough.com>
2008-05-27 19:37:10 -0700

    checkpoint articles_controller_spec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -3,8 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Admin::ArticlesController do
   scenario :basic
 
-  setup do
-    use_controller Admin::ArticlesController
+  before do
     login_as :admin
   end
 
