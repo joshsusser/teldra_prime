3e2951ea3233e8cde32d99584f8e4f16e896e78c
Josh Susser <josh@hasmanythrough.com>
2008-05-29 15:10:01 -0700

    checkpoint

--- test/functional/admin/comments_controller_test.rb
+++ test/functional/admin/comments_controller_test.rb
@@ -1,22 +1,23 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
-describe "Admin::CommentsController" do
+class Admin::CommentsControllerTest < Test::Unit::TestCase
   scenario :blog
 
   setup do
-    use_controller Admin::CommentsController
     login_as :admin
   end
 
-  it "should list all comments in reverse chron order" do
-    get :index
-    assert_response :success
-    assert_nil assigns(:article)
-    assert_not_nil assigns(:comments)
-    assert_equal Comment.count, assigns(:comments).size
+  context "GET /admin/comments/index" do
+    setup { get :index }
+    should_render_template "index"
+    should_assign_to :comments
+    should_not_assign_to :article
+    should "return all comments" do
+      assert_equal Comment.count, assigns(:comments).size
+    end
   end
 
-  it "should list all comments in reverse chron order with pagination" do
+  should "list all comments in reverse chron order with pagination" do
     welcome = articles(:new_release)
     100.times { welcome.comments.create(:author_name => "Bob", :body => "Slack off!") }
     get :index, :page => 2
@@ -26,7 +27,7 @@ describe "Admin::CommentsController" do
     assert_equal Comment.per_page, assigns(:comments).size
   end
 
-  it "should delete a comment" do
+  should "delete a comment" do
     comment = comments(:stalker_welcome)
     assert_difference 'Comment.count', -1 do
       get :destroy, :id => comment.id
@@ -34,5 +35,4 @@ describe "Admin::CommentsController" do
     assert_nil Comment.find_by_id(comment.id)
     assert_response :found
   end
-
 end
