1fa2fa4381106ea9775615c4892ec4010163e5ef
Josh Susser <josh@hasmanythrough.com>
2008-05-28 18:13:50 -0700

    remove test/spec tests

--- test/unit/tagging_test.rb
+++
@@ -1,1 +0,0 @@
-# see tag_test.rb for tests
--- test/unit/user_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-
-class UserTest < ActiveSupport::TestCase
-
-  # def test_has_many_posts
-  #   assert_has_many User, :posts
-  # end
-
-end
