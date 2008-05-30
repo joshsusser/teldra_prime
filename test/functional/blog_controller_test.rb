a83159613616b75fc8ba94cd721797e02b523998
Josh Susser <josh@hasmanythrough.com>
2008-05-30 14:40:03 -0700

    convert blog controller tests

--- test/functional/blog_controller_test.rb
+++ test/functional/blog_controller_test.rb
@@ -1,147 +1,139 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-describe "BlogController" do
+class BlogControllerTest < ActionController::TestCase
   scenario :blog
 
-  setup do
-    use_controller BlogController
-  end
+  context "main blog view (index)" do
+    setup { get :index }
 
-  it "should show archives in index view" do
-    get :index
-    assert_not_nil assigns(:archives)
-    assert_select "ul#archives" do
-      assert_select "li a[href=#{post_path(*articles(:welcome).post_path_params)}]"
+    should_assign_to :articles
+    should "show 5 most recent articles in reverse chron order" do
+      assert_equal 5, assigns(:articles).size
+      assert_sorted(assigns(:articles)) { |a,b| b.published_at <=> a.published_at }
     end
-  end
 
-  it "should not show meta section on Page view" do
-    about = articles(:about)
-    post :page, :slug => about.slug
-    assert_response :success
-    assert_template 'show'
-    assert_select 'ul.meta', false
-  end
-
-  it "should show meta section on Post view" do
-    welcome = articles(:welcome)
-    year, month, day, slug = *welcome.post_path_params
-    post :show, :year => year, :month => month, :day => day, :slug => slug
-    assert_response :success
-    assert_template 'show'
-    assert_select 'p.meta'
-  end
- 
-  it "should not show comments on Page view" do
-    about = articles(:about)
-    post :page, :slug => about.slug
-    assert_response :success
-    assert_template 'show'
-    assert_select 'ol#comments', false
-  end
-  
- it "should show comments on Post view" do
-    welcome = articles(:welcome)
-    year, month, day, slug = *welcome.post_path_params
-    post :show, :year => year, :month => month, :day => day, :slug => slug
-    assert_response :success
-    assert_template 'show'
-    assert_select 'ol#comments'
-  end
-
-  it "should list all tags that have articles" do
-    tags = Tag.find_all_popular
-    get :tags
-    assert_equal tags, assigns(:tags)
-  end
-  
-  it "should list recent articles for a tag" do
-    tag = tags(:meta)
-    get :tag, :tag => tag.name
-    assert_equal tag.articles.recent, assigns(:articles)
-  end
-
-end
-
-describe "BlogController sidebar" do
-  scenario :blog
-
-  setup do
-    use_controller BlogController
-  end
-  
-  it "should show search box" do
-    get :index
-    assert_select "div#search input#q"
-  end
-  
-  it "should return search results" do
-    get :search, :q => "body"
-    assert_template "results"
-    assert_not_nil assigns(:articles)
-  end
-
-  it "should return empty results for blank query" do
-    get :search, :q => ""
-    assert_template "results"
-    assert_equal [], assigns(:articles)
-  end
-
-  it "should show tags in tag cloud" do
-    get :index
-    assert_not_nil assigns(:tag_cloud)
-    assert_select "p#tags"
-  end
-end
-
-describe "BlogController entering new comment" do
-  scenario :blog
-
-  setup do
-    use_controller BlogController
-  end
-
-  it "should create new comment on Post view" do
-    update_post = articles(:update)
-    assert_difference('Comment.count') do
-      assert_difference('update_post.comments.count') do
-        post :create_comment, :article_id => update_post.to_param, :comment => comment_options
+    should_assign_to :archives
+    should "show archive list in reverse chron order" do
+      assert_sorted(assigns(:archives)) { |a,b| b.published_at <=> a.published_at }
+      assert_select "ul#archives" do
+        assert_select "li a[href=#{post_path(*articles(:welcome).post_path_params)}]"
       end
     end
 
-    assert_redirected_to post_path(*assigns(:article).post_path_params)
+    should "show sidebar search box" do
+      assert_select "div#search input#q"
+    end
 
-    # should be follow_redirect
-    ps = update_post.post_path_params
-    get :show, :year => ps[0], :month => ps[1], :day => ps[2], :slug => ps[3]
-    
-    new_comment = Comment.find_by_author_name(comment_options[:author_name])
-    assert !new_comment.author_ip.blank?
-    assert_select "li#comment_#{new_comment.id}"
+    should "show tags in tag cloud" do
+      assert_not_nil assigns(:tag_cloud)
+      assert_select "p#tags"
+    end
   end
-  
-  it "should not create new comment on spammy submission" do
-    update_post = articles(:update)
-    assert_no_difference('Comment.count') do
-      assert_no_difference('update_post.comments.count') do
-        assert_difference('Reject.count') do
-          post :create_comment, :article_id => update_post.to_param, :comment => comment_options(:body => "spam spam spam")
+
+  context "show Page" do
+    setup do
+      about = articles(:about)
+      get :page, :slug => about.slug
+    end
+    should_render_template "show"
+    should "not show meta section" do
+      assert_select 'ul.meta', false
+    end
+    should "not show comments" do
+      assert_select 'ol#comments', false
+    end
+  end
+
+  context "show Post" do
+    setup do
+      welcome = articles(:welcome)
+      year, month, day, slug = *welcome.post_path_params
+      post :show, :year => year, :month => month, :day => day, :slug => slug
+    end
+    should_render_template "show"
+    should "show meta section" do
+      assert_select 'p.meta'
+    end
+    should "show comments" do
+      assert_select 'ol#comments'
+    end
+  end
+
+  context "tags" do
+    should "list all tags that have articles"do
+      get :tags
+      assert_equal Tag.find_all_popular, assigns(:tags)
+    end
+    should "list recent articles for a tag" do
+      tag = tags(:meta)
+      get :tag, :tag => tag.name
+      assert_equal tag.articles.recent, assigns(:articles)
+    end
+  end
+
+  context "searching" do
+    context "valid query" do
+      setup { get :search, :q => "body" }
+      should_render_template "results"
+      should "return non-empty results" do
+        assert !assigns(:articles).empty?
+      end
+    end
+    context "empty query" do
+      setup { get :search, :q => "" }
+      should_render_template "results"
+      should "return non-empty results" do
+        assert_equal [], assigns(:articles)
+      end
+    end
+  end
+
+  context "entering new comment" do
+    should "create new comment on a post" do
+      update_post = articles(:update)
+      assert_difference('Comment.count') do
+        assert_difference('update_post.comments.count') do
+          post :create_comment, :article_id => update_post.to_param, :comment => comment_options
         end
       end
-    end
-  
-    assert_redirected_to post_path(*assigns(:article).post_path_params)
-  end
-  
-  it "should set user_id of logged in author on comment creation" do
-    update_post = articles(:update)
-    
-    post :create_comment, :article_id => update_post.to_param, :comment => comment_options
-    assert_nil assigns(:comment).user_id
 
-    login_as(:admin)
-    post :create_comment, :article_id => update_post.to_param, :comment => comment_options
-    assert_equal users(:admin).id, assigns(:comment).user_id
+      assert_redirected_to post_path(*assigns(:article).post_path_params)
+
+      # should be follow_redirect
+      ps = update_post.post_path_params
+      get :show, :year => ps[0], :month => ps[1], :day => ps[2], :slug => ps[3]
+
+      new_comment = Comment.find_by_author_name(comment_options[:author_name])
+      assert !new_comment.author_ip.blank?
+      assert_select "li#comment_#{new_comment.id}"
+    end
+
+    should "not create new comment on spammy submission" do
+      update_post = articles(:update)
+      assert_no_difference('Comment.count') do
+        assert_no_difference('update_post.comments.count') do
+          assert_difference('Reject.count') do
+            post :create_comment, :article_id => update_post.to_param, :comment => comment_options(:body => "spam spam spam")
+          end
+        end
+      end
+
+      assert_redirected_to post_path(*assigns(:article).post_path_params)
+    end
+
+    should "not set user_id when nobody logged in" do
+      logout
+      update_post = articles(:update)
+      post :create_comment, :article_id => update_post.to_param, :comment => comment_options
+      assert_nil assigns(:comment).user_id
+    end
+
+    should "set user_id of logged in author on comment creation" do
+      login_as(:admin)
+      update_post = articles(:update)
+      post :create_comment, :article_id => update_post.to_param, :comment => comment_options
+      assert_equal users(:admin).id, assigns(:comment).user_id
+    end
   end
 
   def comment_options(options = {})
@@ -151,38 +143,32 @@ describe "BlogController entering new comment" do
       :tofu => "Slack off! Quit your job!"
     }.merge(options)
   end
-end
-
-describe "BlogController feed" do
-  scenario :blog
 
   ATOM_CONTENT_TYPE = "application/atom+xml; charset=utf-8"
 
-  setup do
-    use_controller BlogController
-  end
+  context "feed" do
+    context "content" do
+      setup { get :index, :format => 'atom' }
+      should "respond to main atom feed request with atom xml document" do
+        assert_equal ATOM_CONTENT_TYPE, @response.headers["type"]
+        assert_select "entry", assigns(:articles).size
+      end
+      should_not_assign_to :archives
+      should_not_assign_to :tag_cloud
+    end
 
-  it "should respond to main atom feed request with atom xml document" do
-    get :index, :format => 'atom'
-    assert_equal ATOM_CONTENT_TYPE, @response.headers["type"]
-    assert_select "entry", assigns(:articles).size
-  end
+    context "request for atom tag feed" do
+      setup { get :tag, :tag => 'meta', :format => 'atom' }
+      should "respond with contents" do
+        assert_equal ATOM_CONTENT_TYPE, @response.headers["type"]
+        assert_select "entry", assigns(:articles).size
+      end
+    end
 
-  it "should respond to tag atom feed request with atom xml document" do
-    get :tag, :tag => 'meta', :format => 'atom'
-    assert_equal ATOM_CONTENT_TYPE, @response.headers["type"]
-    assert_select "entry", assigns(:articles).size
-  end
-
-  it "should respond with 404 for a non-existent tag feed" do
-    get :tag, :tag => 'meat', :format => 'atom'
-    assert_response :not_found
-  end
-  
-  it "should not load sidebar data for atom feeds" do
-    get :index, :format => 'atom'
-    assert_nil assigns(:tag_cloud)
-    assert_nil assigns(:archives)
+    context "request for non-existent tag feed" do
+      setup { get :tag, :tag => 'meat', :format => 'atom' }
+      should_respond_with :not_found
+    end
   end
 
 end
