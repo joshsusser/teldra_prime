60c6ddf724635f438761f32172f5d7f9f368b53c
Josh Susser <josh@hasmanythrough.com>
2008-05-31 11:06:58 -0700

    add panel tests

--- test/functional/admin/panel_controller_test.rb
+++ test/functional/admin/panel_controller_test.rb
@@ -1,5 +1,18 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
 class Admin::PanelControllerTest < ActionController::TestCase
+  scenario :blog
 
+  def test_should_authenticate
+    logout
+    get :index
+    assert_redirected_to login_url()
+  end
+
+  def test_should_show_panel
+    login_as :admin
+    get :index
+    assert_response :success
+    assert_template "index"
+  end
 end
